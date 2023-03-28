2ff4f121fa08cab86877ed6d46f355343f6dbc41
mschwab013 <112970959+mschwab013@users.noreply.github.com>
2023-03-28 14:30:13 -0400

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023tnkn",
+      "defaultValue": "2023ohcl",
       "required": "true",
       "disabled": "true"
     },
